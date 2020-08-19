7d9e0130e920914975d62cda3274b53874a4bdad
Eli <bigwisdom1996@gmail.com>
2020-08-19 07:47:13 -0600

    Inventory back fill

--- lib/routes/inventory/inventory.dart
+++ lib/routes/inventory/inventory.dart
@@ -1,108 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get_that_bread/services/data_service/data_service.dart';
 import 'package:get_that_bread/widgets/counter.dart';
+import 'package:provider/provider.dart';
 
 class Inventory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    DataService dataService = Provider.of<DataService>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Inventory"),
       ),
-      body: ListView(
+      body: ListView.builder(
+        itemCount: dataService.shoppingList.length,
         padding: EdgeInsets.only(bottom: 100.0),
-        children: <Widget>[
-          Card(
+        itemBuilder: (context, index) {
+          return Card(
             child: ListTile(
               leading: Icon(Icons.texture),
-              title: Text("Eggs"),
+              title: Text(dataService.shoppingList[index].name),
               trailing: Counter(),
             ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("Milk"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("Cheese"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("BREAAAADD"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("Milk"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("Cheese"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("BREAAAADD"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("Milk"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("Cheese"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("BREAAAADD"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("Milk"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("Cheese"),
-              trailing: Counter(),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.texture),
-              title: Text("BREAAAADD"),
-              trailing: Counter(),
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
